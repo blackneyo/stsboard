c5a9792323391b68eff41f143139f6febf3b600c
이동한 <87853267+blackneyo@users.noreply.github.com>
2022-07-27 17:48:42 +0900

    5

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
 	private final QuestionService questionService;
 	
-	@RequestMapping("/question/list")
+	@RequestMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
