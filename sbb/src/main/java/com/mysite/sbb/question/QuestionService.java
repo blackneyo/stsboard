9e5fa54f6eeb2f4faddbafb8f6dd8bf700e1905b
이동한 <87853267+blackneyo@users.noreply.github.com>
2022-08-02 09:45:55 +0900

    2-15

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,10 +1,13 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
+
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
-import java.util.Optional;
 import com.mysite.sbb.DataNotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,5 +26,12 @@ public class QuestionService {
 		}else {
 			throw new DataNotFoundException("question not found");
 		}
+	}
+	public void create(String subject, String content) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
 	}
 }
