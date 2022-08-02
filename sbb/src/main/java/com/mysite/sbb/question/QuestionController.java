e881cffe534961facd97bf74bc6267f34f481f99
이동한 <87853267+blackneyo@users.noreply.github.com>
2022-08-02 09:09:54 +0900

    2-15

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -28,5 +28,9 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }    
+    @GetMapping("/create")
+    public String questionCreate() {
+    	return "question_form";
     }
 }
